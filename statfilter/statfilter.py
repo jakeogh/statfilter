14e7454fe5ade492027ddcc4e880b99089976b4f
jakeogh <github.com@v6y.net>
2019-12-18 01:25:14 -0700

    auto-commit

--- statfilter/statfilter.py
+++ statfilter/statfilter.py
@@ -11,14 +11,50 @@ ic.lineWrapWidth, _ = get_terminal_size((80, 20))
 #ic.disable()
 
 
+#'st_atime'
+#'st_atime_ns'
+#'st_blksize'
+#'st_blocks'
+#'st_ctime'
+#'st_ctime_ns'
+#'st_dev'
+#'st_gid'
+#'st_ino'
+#'st_mode'
+#'st_mtime'
+#'st_mtime_ns'
+#'st_nlink'
+#'st_rdev'
+#'st_size'
+#'st_uid'
+
+
 # DONT CHANGE FUNC NAME
 @click.command()
 @click.option("--size", type=int)
-def cli(size):
+@click.option("--min-mtime", type=int)
+@click.option("--max-mtime", type=int)
+@click.option("--null", is_flag=True)  # todo
+def cli(size, min_mtime, max_mtime, null):
+
     for line in sys.stdin:
         line = line[:-1]
-        if os.stat(line).st_size >= size:
-            print(line)
+
+        stat = os.stat(line)
+
+        if size:
+            if stat.st_size < size:
+                continue
+
+        if min_mtime:
+            if stat.mtime < min_mtime:
+                continue
+
+        if min_mtime:
+            if stat.mtime > max_mtime:
+                continue
+
+        print(line)
 
 
 if __name__ == "__main__":
