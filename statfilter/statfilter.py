688b8898ab028f6e9f6f743e2e260b117bb304e0
jakeogh <github.com@v6y.net>
2019-12-17 22:49:54 -0700

    auto-commit

---
+++ statfilter/statfilter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import click
+from pathlib import Path
+from icecream import ic
+ic.configureOutput(includeContext=True)
+from shutil import get_terminal_size
+ic.lineWrapWidth, _ = get_terminal_size((80, 20))
+#ic.disable()
+
+
+# DONT CHANGE FUNC NAME
+@click.group()
+@click.option("--size", type=int)
+def cli(size):
+    for line in sys.stdin:
+        line = line[:-1]
+        if os.stat(line).st_size >= size:
+            print(line)
+
+
+if __name__ == "__main__":
+    cli()
