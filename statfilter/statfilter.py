8e282d9bee8e6e1881a733905471d6b1eb323353
jakeogh <github.com@v6y.net>
2020-04-03 12:28:40 -0700

    auto-commit

--- statfilter/statfilter.py
+++ statfilter/statfilter.py
@@ -40,11 +40,11 @@ def read_by_byte(file_object, byte):    # by ikanobori
         ic(sep, len(buf))
 
         while sep != -1:
-            sep_end_marker = len(buf) - 1
-            ic(sep_end_marker)
-            if sep == sep_end_marker:
-                ic(sep, "return")
-                return
+            #sep_end_marker = len(buf) - 1
+            #ic(sep_end_marker)
+            #if sep == sep_end_marker:
+            #    ic(sep, "return")
+            #    return
 
             ret, buf = buf[:sep], buf[sep + 1:]
             yield ret
