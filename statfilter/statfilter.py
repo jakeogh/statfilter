7a5298018ce6aeff0d467b95d4992b2feffd41dd
jakeogh <github.com@v6y.net>
2020-04-03 12:16:24 -0700

    auto-commit

--- statfilter/statfilter.py
+++ statfilter/statfilter.py
@@ -37,6 +37,7 @@ def read_by_byte(file_object, byte):    # by ikanobori
         ic(len(chunk))
         buf += chunk
         sep = buf.find(byte)
+        ic(sep)
 
         while sep != -1:
             if sep == len(buf) - 1:
